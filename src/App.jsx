3aa4447292486f6cb81764870e28cb66ac2a9253
melvinchia3636 <melvinchia623600@gmail.com>
2023-03-05 14:31:45 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
+
+const TaskContainer = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+`;
+
+const initialData = {
+  tasks: {
+    "task-1": { id: "task-1", content: "Take out the garbage" },
+    "task-2": { id: "task-2", content: "Watch my favorite show" },
+    "task-3": { id: "task-3", content: "Charge my phone" },
+    "task-4": { id: "task-4", content: "Cook dinner" },
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "To do",
+      taskIds: ["task-1", "task-2", "task-3", "task-4"],
+    },
+  },
+  // Facilitate reordering of the columns
+  columnOrder: ["column-1"],
+};
+
+function Task({ task, index }) {
+  return (
+    <Draggable draggableId={task.id} index={index}>
+      {(provided) => (
+        <div
+          className="h-[4.2rem]"
+          {...provided.draggableProps}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <div className="bg-slate-50 p-4 shadow-md rounded-md">
+            {task.content}
+          </div>
+        </div>
+      )}
+    </Draggable>
+  );
+}
+
+function Column({ column, tasks }) {
+  return (
+    <div className="bg-white rounded-lg shadow-lg p-4 w-full md:w-1/2">
+      <h1 className="text-center text-2xl font-semibold uppercase tracking-widest">
+        {column.title}
+      </h1>
+      <Droppable droppableId={column.id}>
+        {(provided) => (
+          <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+            {tasks.map((task, index) => (
+              <Task key={task.id} task={task} index={index} />
+            ))}
+            {provided.placeholder}
+          </TaskList>
+        )}
+      </Droppable>
+    </div>
+  );
+}
+
+function App() {
+  const [state, setState] = useState(initialData);
+
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    setState(newState);
+  };
+
+  return (
+    <main className="w-full h-screen flex items-center justify-center bg-slate-100 text-slate-700 p-12">
+      <DragDropContext onDragEnd={onDragEnd}>
+        {state.columnOrder.map((columnId) => {
+          const column = state.columns[columnId];
+          const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
+
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
+    </main>
+  );
+}
+
+export default App;
